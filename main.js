1983ac88c913b3f046b6030ecc4afd919a834b83
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2022-05-17 16:16:01 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const menu = document.querySelector('.menu');
+const close = document.querySelector('.close');
+const navigation = document.querySelector('.navigation');
+
+menu.addEventListener('click', () => {
+    // Hide the hamburger icon and show the close icon
+    menu.style.display = "none";
+    close.style.display = "block";
+
+    // Show the menu
+    navigation.style.display = "block";
+});
+
+close.addEventListener('click', () => {
+    close.style.display = "none";
+    menu.style.display = "block";
+
+    // Hide the menu
+    navigation.style.display = "none";
+});
